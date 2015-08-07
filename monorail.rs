8f046e511df395e0de787b38f414a9d8cbde76da
Peter Tseng <pht24@cornell.edu>
2015-08-07 01:32:47 -0700

    Implement board type

--- monorail.rs
+++ monorail.rs
@@ -28,6 +28,12 @@ impl Coordinate {
             Direction::Right => Coordinate{row: self.row, col: self.col + delta},
         }
     }
+    fn induces_board_type(&self) -> bool {
+        *self == Coordinate{row: 1, col: 1} ||
+        *self == Coordinate{row: 2, col: 1} ||
+        *self == Coordinate{row: 2, col: 0} ||
+        *self == Coordinate{row: 3, col: 0}
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -106,6 +112,11 @@ enum BoardType {
     Middle,
     Right,
 }
+const POSSIBLE_BOARD_TYPES: [BoardType; 3] = [
+    BoardType::Left,
+    BoardType::Middle,
+    BoardType::Right,
+];
 
 type BoardArray = [[bool; NUM_COLS]; NUM_ROWS];
 struct Board {
@@ -115,10 +126,15 @@ struct Board {
 
 impl Board {
     fn make_move(&mut self, m: Move) {
+        match m.board_type {
+            Some(_) => self.board_type = m.board_type,
+            None => (),
+        }
         self.set_squares(m, true)
     }
 
-    fn undo_move(&mut self, m: Move) {
+    fn undo_move(&mut self, m: Move, bt: Option<BoardType>) {
+        self.board_type = bt;
         self.set_squares(m, false)
     }
 
@@ -159,13 +175,13 @@ impl Board {
     }
 
     // Unfortunately I kind of have to hard-code this.
-    fn makes_board_unsolvable(&self, c: Coordinate) -> bool {
-        (self.occupied(Coordinate{row: 3, col: 0}) && c == Coordinate{row: 2, col: 1}) ||
-        (self.occupied(Coordinate{row: 3, col: 0}) && c == Coordinate{row: 1, col: 1}) ||
-        (self.occupied(Coordinate{row: 2, col: 0}) && c == Coordinate{row: 1, col: 1}) ||
-        (self.occupied(Coordinate{row: 2, col: 1}) && c == Coordinate{row: 3, col: 0}) ||
-        (self.occupied(Coordinate{row: 1, col: 1}) && c == Coordinate{row: 2, col: 0}) ||
-        (self.occupied(Coordinate{row: 1, col: 1}) && c == Coordinate{row: 3, col: 0})
+    fn makes_unsolvable(c: Coordinate, b: Option<BoardType>) -> bool {
+        match b {
+            Some(BoardType::Left)   => c == Coordinate{row: 2, col: 1} || c == Coordinate{row: 1, col: 1},
+            Some(BoardType::Middle) => c == Coordinate{row: 3, col: 0} || c == Coordinate{row: 1, col: 1},
+            Some(BoardType::Right)  => c == Coordinate{row: 3, col: 0} || c == Coordinate{row: 2, col: 0},
+            None => false,
+        }
     }
 
     fn frontier(&self) -> Vec<Coordinate> {
@@ -173,7 +189,7 @@ impl Board {
         for row in 0..NUM_ROWS {
             for col in 0..NUM_COLS {
                 let coord = Coordinate{row: row, col: col};
-                if self.occupied(coord) || self.makes_board_unsolvable(coord) {
+                if self.occupied(coord) || Board::makes_unsolvable(coord, self.board_type) {
                     continue;
                 }
                 let mut have_neighbor = false;
@@ -196,20 +212,42 @@ impl Board {
         let mut results = Vec::new();
         for frontier_space in self.frontier().iter() {
             for move_type in POSSIBLE_MOVE_TYPES.iter() {
-                // TODO: fix board_type
                 let mov = Move{coord: *frontier_space, move_type: *move_type, board_type: None};
                 if !self.move_in_bounds(mov) {
                     continue;
                 }
                 let mut other_space_taken = false;
+                let mut induces_board_type = frontier_space.induces_board_type();
                 for other_space in mov.coords().iter() {
-                    if self.occupied(*other_space) || self.makes_board_unsolvable(*other_space) {
+                    if other_space.induces_board_type() {
+                        induces_board_type = true;
+                    }
+                    if self.occupied(*other_space) {
                         other_space_taken = true;
                         break;
                     }
                 }
                 if !other_space_taken {
-                    results.push(mov);
+                    if induces_board_type && self.board_type.is_none() {
+                        for board_type in POSSIBLE_BOARD_TYPES.iter() {
+                            if Board::makes_unsolvable(*frontier_space, Some(*board_type)) {
+                                continue;
+                            }
+                            let mut other_spaces_ok = true;
+                            for other_space in mov.coords().iter() {
+                                if Board::makes_unsolvable(*other_space, Some(*board_type)) {
+                                    other_spaces_ok = false;
+                                    break;
+                                }
+                            }
+                            if other_spaces_ok {
+                                results.push(Move{coord: mov.coord, move_type: mov.move_type, board_type: Some(*board_type)});
+                            }
+                        }
+
+                    } else {
+                        results.push(mov);
+                    }
                 }
             }
         }
@@ -223,6 +261,7 @@ impl Board {
             }
             println!("");
         }
+        println!("{:?}", self.board_type);
     }
 }
 
@@ -253,9 +292,10 @@ fn minimax_alpha_beta(player: Player, board: &mut Board, initial_alpha: GameResu
     let mut best_move = None;
 
     for possible_move in moves.iter() {
+        let bt = board.board_type;
         board.make_move(*possible_move);
         let (reply, _) = minimax_alpha_beta(player.opponent(), board, alpha, beta);
-        board.undo_move(*possible_move);
+        board.undo_move(*possible_move, bt);
 
         match player {
             Player::YeonSeung => {
@@ -327,6 +367,7 @@ fn main() {
     if all_responses {
         for legal_move in starting_board.legal_moves().iter() {
             print!("{:?} does: {:?}, ", starting_player, legal_move);
+            let bt = starting_board.board_type;
             starting_board.make_move(*legal_move);
             let (result, best_move) = minimax_alpha_beta(starting_player.opponent(), &mut starting_board, GameResult::PlaceholderJunSeok, GameResult::PlaceholderYeonSeung);
             match best_move {
@@ -334,11 +375,11 @@ fn main() {
                     println!("{:?} does: {:?}, {:?}", starting_player.opponent(), x, result);
                     starting_board.make_move(x);
                     starting_board.print();
-                    starting_board.undo_move(x);
+                    starting_board.undo_move(x, bt);
                 }
                 None => (),
             }
-            starting_board.undo_move(*legal_move);
+            starting_board.undo_move(*legal_move, bt);
         }
     }
 
